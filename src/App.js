f83af64ef6b4ad3deb01c628d8a447b1a911a3e1
Jiackie <Jiaqili1023@gmail.com>
2020-06-09 22:44:08 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
+function Box(props) {
+
+  let color = 'black';
+  if (props.cur.filter(item => item.col === props.c && item.row === props.r).length !== 0) color = 'current';
+  if (props.fill.filter(item => item.col === props.c && item.row === props.r).length !== 0) color = 'filled';
+  console.log(color, props.c, props.r);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={`box blk-bkg ${color === 'current' ? 'yello-bkg': ''} ${color ==='filled' ? 'wt-bkg': ''}`}></div>
+  );
+}
+
+function App(props) {
+  const [target, setTarget] = useState(0);
+  const [status, setStatus] = useState(0);
+  const [cur, setCur] = useState(props.allBox[0][0]);
+  const [fill, setFill] = useState(props.allBox[1][1]);
+  console.log(props.allBox, 'box', cur);
+
+  // useEffect(() => {
+  //   window.addEventLister('key')
+  // },[]);
+  
+  useEffect(() => {
+    setInterval(() => {
+      
+    },1000)
+  }, [cur])
+
+  return (
+    <div className="app">
+      <header>
+        <div className="score">
+          Score: 
+          <div>1</div>
+        </div>
       </header>
+      <div className="grid">
+        {new Array(15).fill(1).map((vr, r) => (
+          <div className="row" key={'row' + r}>
+            {new Array(13).fill(1).map((vc, c) => (
+              <Box c={c} r ={r} cur={cur} fill={fill} key={'col' + c + 'row' + r} />
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+// add next sign 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const allBox =
+  [
+    [
+      [
+        {'row':0,'col':1}, {'row':1,'col':0}, {'row':1,'col':1}, {'row':1,'col':2}
+      ],
+      [
+        {'row':0,'col':0}, {'row':1,'col':0}, {'row':1,'col':1}, {'row':2,'col':0}
+      ],
+      [
+        {'row':0,'col':0}, {'row':0,'col':1}, {'row':0,'col':2}, {'row':1,'col':1}
+      ],
+      [
+        {'row':0,'col':1}, {'row':1,'col':0}, {'row':1,'col':1}, {'row':2,'col':1}
+      ]
+    ],
+    [
+      [
+        {'row':0,'col':0}
+      ],
+      [
+        {'row':0,'col':0}
+      ],
+      [
+        {'row':0,'col':0}
+      ],
+      [
+        {'row':0,'col':0}
+      ]
+    ]
+  ];
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App allBox={allBox}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
